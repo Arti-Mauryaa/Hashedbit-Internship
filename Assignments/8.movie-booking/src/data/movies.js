8d3f7fe36e7fdefa24b3b16c9d81a73d9d9d22ce
Arti Mauryaa <artimaurya9721@gmail.com>
2026-06-22 17:07:00 +0530

    add assignment

--- Assignments/8.movie-booking/src/data/movies.js
+++ Assignments/8.movie-booking/src/data/movies.js
@@ -2,49 +2,49 @@ const movies = [
   {
     id: 1,
     title: "Avengers",
-    image: "https://picsum.photos/200?1",
+    image: "https://media.istockphoto.com/id/1007557230/photo/movie-projector-on-dark-background.jpg?s=612x612&w=0&k=20&c=bPgXKrqKU4nrWgJ_RXgLe9KN4SIitgTIcRobxxBiXTo=",
     description: "Marvel Superhero Movie"
   },
   {
     id: 2,
     title: "Batman",
-    image: "https://picsum.photos/200?2",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSjjym3HovYT7cXNaoYJ1whYcNCY8kFfF_eg&s",
     description: "DC Action Movie"
   },
   {
     id: 3,
     title: "Spiderman",
-    image: "https://picsum.photos/200?3",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSjjym3HovYT7cXNaoYJ1whYcNCY8kFfF_eg&s",
     description: "Friendly Neighborhood Hero"
   },
   {
     id: 4,
     title: "Joker",
-    image: "https://picsum.photos/200?4",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSjjym3HovYT7cXNaoYJ1whYcNCY8kFfF_eg&s",
     description: "Psychological Thriller"
   },
   {
     id: 5,
     title: "Titanic",
-    image: "https://picsum.photos/200?5",
+    image: "https://media.istockphoto.com/id/1007557230/photo/movie-projector-on-dark-background.jpg?s=612x612&w=0&k=20&c=bPgXKrqKU4nrWgJ_RXgLe9KN4SIitgTIcRobxxBiXTo=",
     description: "Romantic Drama"
   },
   {
     id: 6,
     title: "Inception",
-    image: "https://picsum.photos/200?6",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSjjym3HovYT7cXNaoYJ1whYcNCY8kFfF_eg&s",
     description: "Sci-Fi Thriller"
   },
   {
     id: 7,
     title: "Interstellar",
-    image: "https://picsum.photos/200?7",
+    image: "https://media.istockphoto.com/id/1007557230/photo/movie-projector-on-dark-background.jpg?s=612x612&w=0&k=20&c=bPgXKrqKU4nrWgJ_RXgLe9KN4SIitgTIcRobxxBiXTo=",
     description: "Space Adventure"
   },
   {
     id: 8,
     title: "Avatar",
-    image: "https://picsum.photos/200?8",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSjjym3HovYT7cXNaoYJ1whYcNCY8kFfF_eg&s",
     description: "Fantasy Movie"
   }
 ];
